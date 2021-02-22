62e5e2b171447df5e365e771d00601fc9d0eaff6
Duvey <rudow.david@gmail.com>
2021-02-22 11:57:37 -0800

    assets completed

---
+++ Tutorial 2/New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{   
+    public float horizontalInput; 
+    public float verticalInput;
+    public float speed = 10;
+    public float xRange = 10;
+    public float zRange = 12;
+
+    public GameObject projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -xRange){
+
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+            
+        }
+        
+        else if (transform.position.x > xRange){
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.z < 0){
+
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+            
+        }
+
+        else if (transform.position.z > zRange){
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)){
+            Instantiate(projectilePrefab,transform.position, projectilePrefab.transform.rotation);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+    }
+}
