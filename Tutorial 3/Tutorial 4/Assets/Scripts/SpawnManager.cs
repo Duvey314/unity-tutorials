ffd460c70d741b785e86b3c62a5e2fbc1f8baefe
Duvey <rudow.david@gmail.com>
2021-03-05 18:23:37 -0800

    tutorial 4 started

---
+++ Tutorial 3/Tutorial 4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Instantiate(enemyPrefab, GenerateSpawnPosition() , enemyPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Vector3 GenerateSpawnPosition(){
+        float spawnPositionx = Random.Range(-9, 9);
+        float spawnPositionz = Random.Range(-9, 9);
+
+        Vector3 randomPos = new Vector3(spawnPositionx,0,spawnPositionz);
+
+        return randomPos;
+    }
+}
