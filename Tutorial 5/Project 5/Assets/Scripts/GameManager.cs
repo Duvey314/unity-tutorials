3bf7851ccea3ece86fb7edf1d059eb8b0cd2aa32
Duvey <rudow.david@gmail.com>
2021-03-16 16:56:56 -0700

    unity toturial 5

---
+++ Tutorial 5/Project 5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{   
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+
+    public bool isGameActive;
+
+    public Button resetButton;
+    private int score = 0; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget(){
+        while (isGameActive){
+           yield return new WaitForSeconds(spawnRate);
+           int index = Random.Range(0, targets.Count); 
+           Instantiate(targets[index]);
+        }
+        
+    }
+
+    public void UpdateScore(int scoreToAdd){
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver(bool yes){
+        gameOverText.gameObject.SetActive(yes);
+        isGameActive = false;
+        resetButton.gameObject.SetActive(true);
+    }
+
+    public void RestartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Tutorial 5/Project 5/Assets/Scripts/Target.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+    private Rigidbody targetRB;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float torqueRange = 10;
+    private float xStartRange = 4;
+    private float yStart = 2;
+
+    public int pointValue;
+
+    public ParticleSystem explosionParticle;
+
+    private GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the rigid body component
+        targetRB = GetComponent<Rigidbody>();
+
+        // Set random values for the force and torque
+        targetRB.AddForce(Vector3.up * Random.Range(minSpeed,maxSpeed), ForceMode.Impulse);
+        targetRB.AddTorque(Random.Range(-torqueRange,torqueRange),Random.Range(-torqueRange,torqueRange),Random.Range(-torqueRange,torqueRange), ForceMode.Impulse);
+        transform.position = new Vector3(Random.Range(-xStartRange,xStartRange), -yStart);
+
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown(){
+        if(gameManager.isGameActive){
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue); 
+        } 
+    }
+
+    private void OnTriggerEnter(Collider other){
+        Destroy(gameObject);
+        if(!gameObject.CompareTag("Bad")){
+          gameManager.GameOver(true); 
+        }
+        
+    }
+}
