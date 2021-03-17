8e7a24cb35d499be342706feb2f5d5556ee2bbff
Duvey <rudow.david@gmail.com>
2021-03-17 10:39:32 -0700

    tutorial 5 completed

--- Tutorial 5/Project 5/Assets/Scripts/GameManager.cs
+++ Tutorial 5/Project 5/Assets/Scripts/GameManager.cs
@@ -8,18 +8,31 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {   
     public List<GameObject> targets;
-    private float spawnRate = 1.0f;
+    
+    // Create text variables
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI difficultyText;
 
-    public bool isGameActive;
-
+    // Create buttons
     public Button resetButton;
-    private int score = 0; 
+    public Button easyButton;
+    public Button mediumButton;
+    public Button hardButton;
+
+
+    // Initialize variables
+    private int score = 0;
+    private float spawnRate = 1.0f;
+    public bool isGameActive;
     // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true;
+        isGameActive = false;
+        easyButton.gameObject.SetActive(true);
+        mediumButton.gameObject.SetActive(true);
+        hardButton.gameObject.SetActive(true);
+        difficultyText.gameObject.SetActive(true);
         StartCoroutine(SpawnTarget());
         UpdateScore(0);
         
@@ -53,5 +66,23 @@ public class GameManager : MonoBehaviour
 
     public void RestartGame(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void SelectDifficulty(Button button){
+        if(button.name == "Easy Button"){
+            spawnRate = 1.0f;
+        }
+        else if(button.name == "Medium Button"){
+            spawnRate = 0.75f;
+        }
+        else{
+            spawnRate = 0.5f;
+        }
+        easyButton.gameObject.SetActive(false);
+        mediumButton.gameObject.SetActive(false);
+        hardButton.gameObject.SetActive(false);
+        difficultyText.gameObject.SetActive(false);
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
     }
 }
